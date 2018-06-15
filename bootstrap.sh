950e542b572195e1347c288094c093e9f2b96033
Rob Senn <rsenn@gorillagroup.com>
2018-06-15 14:25:41 -0400

    Fix file path

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,20 +3,28 @@
 
 set -e
 
-# Xcode Command Line Tools
-xcode-select --install || true
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-# Install Homebrew
-brew help &>/dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# # Xcode Command Line Tools
+# xcode-select --install || true
 
-# Install OhMyZsh
-stat ~/.oh-my-zsh &>/dev/null || sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# # Install Homebrew
+# brew help &>/dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install Ansible
-brew install ansible || true
+# # Install OhMyZsh
+# stat ~/.oh-my-zsh &>/dev/null || sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# # Install Ansible
+# brew install ansible || true
 
 # Run the M2 stack playbook
-ansible-playbook --ask-become-pass `dirname "$0"`/m2_install.yml
+ansible-playbook --ask-become-pass $DIR/m2_install.yml
 
 # Secure the MariaDB installation
 # mysql_secure_installation
